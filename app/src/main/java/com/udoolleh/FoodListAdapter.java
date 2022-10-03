e40f35a1965d1ddb077a4431840bb8f4a7dc06cb
sangyoon <qlrqod123123@naver.com>
2022-10-03 16:47:57 +0900

    맛집 리스트 데이터 그리드뷰에 Inflate 성공 및 맛집 리스트 레이아웃 구성, Rounded Box 형태 레이아웃에 Elevation 추가

---
+++ app/src/main/java/com/udoolleh/FoodListAdapter.java
@@ -0,0 +1,70 @@
+package com.udoolleh;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FoodListAdapter extends BaseAdapter {
+    ArrayList<FoodListItem> items = new ArrayList<FoodListItem>();
+    Context context;
+
+    public void addItem(FoodListItem item) {
+        items.add(item);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        context = viewGroup.getContext();
+        FoodListItem foodListItem = items.get(i);
+
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.fragment_food_gridview_item, viewGroup, false);
+        }
+
+        ImageView foodImagesUrl = view.findViewById(R.id.foodImagesUrl);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            foodImagesUrl.setClipToOutline(true);
+        }
+        TextView foodName = view.findViewById(R.id.foodName);
+        //TextView foodPlaceType = view.findViewById(R.id.foodPlaceType);
+        //TextView foodCategory = view.findViewById(R.id.foodCategory);
+        TextView foodAddress = view.findViewById(R.id.foodAddress);
+        TextView foodTotalGrade = view.findViewById(R.id.foodTotalGrade);
+        //TextView foodXcoordinate = view.findViewById(R.id.foodXcoordinate);
+        //TextView foodYcoordinate = view.findViewById(R.id.foodYcoordinate);
+
+        //foodImagesUrl.setText(foodListItem.getImagesUrl());
+        foodName.setText(foodListItem.getName());
+        //foodPlaceType.setText(foodListItem.getPlaceType());
+        //foodCategory.setText(foodListItem.getCategory());
+        foodAddress.setText(foodListItem.getAddress());
+        foodTotalGrade.setText(foodListItem.getTotalGrade());
+        //foodXcoordinate.setText(foodListItem.getXcoordinate());
+        //foodYcoordinate.setText(foodListItem.getYcoordinate());
+
+        return view;
+    }
+}
