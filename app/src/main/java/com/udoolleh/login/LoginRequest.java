74028bc6a73cb0acd221de37f9b5621da4e31c80
sangyoon <qlrqod123123@naver.com>
2022-11-30 04:12:03 +0900

    자바 파일 폴더 분류

---
+++ app/src/main/java/com/udoolleh/login/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.udoolleh.login;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginRequest {
+    @SerializedName("email")
+    public String inputId;
+
+    @SerializedName("password")
+    public String inputPw;
+
+    public String getInputId() {
+        return inputId;
+    }
+
+    public String getInputPw() {
+        return inputPw;
+    }
+
+    public void setInputId(String inputId) {
+        this.inputId = inputId;
+    }
+
+    public void setInputPw(String inputPw) {
+        this.inputPw = inputPw;
+    }
+
+    public LoginRequest(String inputId, String inputPw) {
+        this.inputId = inputId;
+        this.inputPw = inputPw;
+    }
+}
---
+++ app/src/main/java/com/udoolleh/map/Redbus.java
@@ -0,0 +1,16 @@
+package com.udoolleh.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udoolleh.R;
+
+public class Redbus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redbus);
+    }
+}
