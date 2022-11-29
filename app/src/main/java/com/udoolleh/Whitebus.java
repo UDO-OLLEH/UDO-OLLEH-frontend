108ccff7280b33a6cd0b7d60925ead0416349e83
dnscodnjs <yun9869@naver.com>
2022-11-30 00:42:43 +0900

    fragment_map.xml 이미지 추가 (아직 일렬로 나옴), selected_motion 했더니 렌더링 문제
    Whitebus, Redbus, Jongdal, Seongsan 클래스 추가
    
    MapFragment.java intent 화면전환

---
+++ app/src/main/java/com/udoolleh/Whitebus.java
@@ -0,0 +1,14 @@
+package com.udoolleh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Whitebus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whitebus);
+    }
+}
