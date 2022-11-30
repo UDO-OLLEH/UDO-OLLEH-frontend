f14d108eb2f68109d440be3a47756a618e73cf6b
dnscodnjs <yun9869@naver.com>
2022-11-30 12:30:11 +0900

    map_text_selector.xml, map_button_selected_motion.xml - 버튼 선택시 버튼 색과 버튼 글씨 색 변경 xml 추가
    MapFragment.java
    - 각 버튼 선택시 visibility 변경
    
    각 이미지 xml에 확인용으로 이름 추가

--- app/src/main/java/com/udoolleh/MapFragment.java
+++ app/src/main/java/com/udoolleh/MapFragment.java
@@ -1,8 +1,11 @@
 package com.udoolleh;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,18 +16,18 @@ import android.widget.ImageView;
 public class MapFragment extends Fragment {
     Button btnBus, btnBoat;
     ImageView ivRedbus, ivWhitebus, ivJongdal, ivSeongsan;
+    CardView cvRedbus, cvWhitebus, cvJongboat, cvSeongboat;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        //프래그먼트에서 뷰 보내기위함
-        Context context = view.getContext();
 
         btnBus = view.findViewById(R.id.btnBus);
         btnBoat = view.findViewById(R.id.btnBoat);
@@ -33,52 +36,73 @@ public class MapFragment extends Fragment {
         ivJongdal = view.findViewById(R.id.ivJongdal);
         ivSeongsan = view.findViewById(R.id.ivSeongsan);
 
-        btnBus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        cvRedbus = view.findViewById(R.id.cvRedbus);
+        cvWhitebus = view.findViewById(R.id.cvWhitebus);
+        cvJongboat = view.findViewById(R.id.cvJongboat);
+        cvSeongboat = view.findViewById(R.id.cvSeongboat);
 
-            }
-        });
+        btnBus.setSelected(true);
 
-        btnBoat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        ivRedbus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, Redbus.class);
-                startActivity(intent);
-            }
-        });
-
-        ivWhitebus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, Whitebus.class);
-                startActivity(intent);
-            }
-        });
-
-        ivJongdal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, Jongdal.class);
-                startActivity(intent);
-            }
-        });
-
-        ivSeongsan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, Seongsan.class);
-                startActivity(intent);
-            }
-        });
+        btnBus.setOnClickListener(this::onClick);
+        btnBoat.setOnClickListener(this::onClick);
+        ivRedbus.setOnClickListener(this::onClick);
+        ivWhitebus.setOnClickListener(this::onClick);
+        ivJongdal.setOnClickListener(this::onClick);
+        ivSeongsan.setOnClickListener(this::onClick);
 
         return view;
+
+    }
+
+    private void onClick(View view){
+
+        //프래그먼트에서 뷰 보내기위함
+        Context context = view.getContext();
+
+        switch (view.getId()){
+            case R.id.btnBus:
+                btnBoat.setSelected(false);
+                btnBus.setSelected(true);
+
+                cvRedbus.setVisibility(View.VISIBLE);
+                cvWhitebus.setVisibility(View.VISIBLE);
+
+                cvJongboat.setVisibility(View.GONE);
+                cvSeongboat.setVisibility(View.GONE);
+
+                break;
+
+            case R.id.btnBoat:
+                btnBus.setSelected(false);
+                btnBoat .setSelected(true);
+
+                cvRedbus.setVisibility(View.GONE);
+                cvWhitebus.setVisibility(View.GONE);
+
+                cvJongboat.setVisibility(View.VISIBLE);
+                cvSeongboat.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.ivRedbus:
+                Intent intent = new Intent(context, Redbus.class);
+                startActivity(intent);
+                break;
+
+            case R.id.ivWhitebus:
+                Intent intent1 = new Intent(context, Whitebus.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.ivJongdal:
+                Intent intent2 = new Intent(context, Jongdal.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.ivSeongsan:
+                Intent intent3 = new Intent(context, Seongsan.class);
+                startActivity(intent3);
+                break;
+
+        }
     }
 }
