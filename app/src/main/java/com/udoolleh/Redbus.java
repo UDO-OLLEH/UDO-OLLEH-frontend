66ec056038f91aeed083804cee0876f5cea0e529
dnscodnjs <yun9869@naver.com>
2022-12-18 15:58:42 +0900

    redbus ,whitebus, seongsan, jongdal.xml 디자인
    redbus ,whitebus, seongsan, jongdal.java
    버튼 작동

--- app/src/main/java/com/udoolleh/Redbus.java
+++ app/src/main/java/com/udoolleh/Redbus.java
@@ -1,14 +1,40 @@
 package com.udoolleh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Redbus extends AppCompatActivity {
+
+    NestedScrollView scrollView;
+    Button btnRoute, btnTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redbus);
+
+        scrollView = findViewById(R.id.scrollView);
+        btnRoute = findViewById(R.id.btnRoute);
+        btnTime = findViewById(R.id.btnTime);
+
+        btnRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrollView.fullScroll(scrollView.FOCUS_UP);
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrollView.fullScroll(scrollView.FOCUS_DOWN);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/udoolleh/Whitebus.java
+++ app/src/main/java/com/udoolleh/Whitebus.java
@@ -1,14 +1,39 @@
 package com.udoolleh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Whitebus extends AppCompatActivity {
+
+    NestedScrollView scrollView;
+    Button btnRoute, btnTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whitebus);
+
+        scrollView = findViewById(R.id.scrollView);
+        btnRoute = findViewById(R.id.btnRoute);
+        btnTime = findViewById(R.id.btnTime);
+
+        btnRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrollView.fullScroll(scrollView.FOCUS_UP);
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrollView.fullScroll(scrollView.FOCUS_DOWN);
+            }
+        });
+
     }
 }
