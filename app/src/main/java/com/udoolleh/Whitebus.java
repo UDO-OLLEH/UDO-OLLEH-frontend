2992f102d2db371fd08b0c8b9f3d2c7b9c9c837a
dnscodnjs <yun9869@naver.com>
2022-12-23 17:14:47 +0900

    redbus, whitebus, jongdal, seongsan 메뉴기능 추가

--- app/src/main/java/com/udoolleh/Whitebus.java
+++ app/src/main/java/com/udoolleh/Whitebus.java
@@ -1,13 +1,22 @@
 package com.udoolleh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.NestedScrollView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.amar.library.ui.StickyScrollView;
+import com.google.android.material.navigation.NavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class Whitebus extends AppCompatActivity {
 
@@ -22,6 +31,10 @@ public class Whitebus extends AppCompatActivity {
         scrollView = findViewById(R.id.scrollView);
         btnRoute = findViewById(R.id.btnRoute);
         btnTime = findViewById(R.id.btnTime);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         btnRoute.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +50,54 @@ public class Whitebus extends AppCompatActivity {
             }
         });
 
+        //네비게이션 메뉴
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.nav_home:
+                        Toast.makeText(Whitebus.this, "home", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_setting:
+                        Toast.makeText(Whitebus.this, "setting", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nav_example:
+                        Toast.makeText(Whitebus.this, "example", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                DrawerLayout drawer = findViewById(R.id.drawerLayout);
+                drawer.closeDrawer(GravityCompat.END);
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        switch (item.getItemId()){
+            case android.R.id.home: //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            case R.id.drawer:
+                if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                    Toast.makeText(getApplicationContext(), "open", Toast.LENGTH_SHORT).show();
+                } else if (!drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                    drawerLayout.openDrawer(GravityCompat.END);
+                }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
