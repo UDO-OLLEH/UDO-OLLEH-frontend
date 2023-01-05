ee6644dbb2b71e1df97754397c146a3f81acaf0d
Sangyoon <qlrqod123123@naver.com>
2023-01-05 15:53:44 +0900

    MainFragment 추천 관광지 조회, 추천 관광지 상세 조회 통신 구현 완료

--- app/src/main/java/com/udoolleh/MainFragment.java
+++ app/src/main/java/com/udoolleh/MainFragment.java
@@ -11,7 +11,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import org.jsoup.Jsoup;
@@ -35,12 +38,14 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainFragment extends Fragment {
-    Context context;
-    TextView weather, weatherSub;
-    Button recycle;
-    ImageView weather_layout;
     private RetrofitClient retrofitClient;
     private RetrofitInterface retrofitInterface;
+    Context context;
+    RecyclerView tourPlaceRecyclerView;
+    TourFragmentPlaceAdapter tourFragmentPlaceAdapter;
+    TextView weather, weatherSub, noneTourPlaceText;
+    Button recycle;
+    ImageView weather_layout;
     String weatherLink = "https://weather.naver.com/today/14110330";
     private ViewPager2 ad_viewpager_slider;
     MainFragmentAdImageSliderAdapter adImageSliderAdapter;
@@ -58,6 +63,16 @@ public class MainFragment extends Fragment {
         weather = view.findViewById(R.id.weather);
         weatherSub = view.findViewById(R.id.weatherSub);
         weather_layout = view.findViewById(R.id.weather_layout);
+
+        //PlaceRecyclerView
+        tourPlaceRecyclerView = view.findViewById(R.id.tourPlaceRecyclerView);
+        noneTourPlaceText = view.findViewById(R.id.noneTourPlaceText);
+        LinearLayoutManager placeLinearLayoutManager = new LinearLayoutManager(context, RecyclerView.HORIZONTAL, false);
+        tourPlaceRecyclerView.setLayoutManager(placeLinearLayoutManager);
+        tourFragmentPlaceAdapter = new TourFragmentPlaceAdapter();
+
+        //PlaceRetrofit
+        TourPlaceResponse();
 
         //ViewPager
         ad_viewpager_slider = view.findViewById(R.id.ad_viewpager_slider);
@@ -182,6 +197,92 @@ public class MainFragment extends Fragment {
             @Override
             public void onFailure(Call<MainFragmentAdResponse> call, Throwable t) {
 
+            }
+        });
+    }
+
+    public void TourPlaceResponse() {
+        //retrofit 생성
+        retrofitClient = RetrofitClient.getInstance(null);
+        retrofitInterface = RetrofitClient.getRetrofitInterface();
+
+        //TourCourseResponse에 저장된 데이터와 함께 RetrofitInterface에서 정의한 getCourseSesponse 함수를 실행한 후 응답을 받음
+        retrofitInterface.getTourFragmentPlaceResponse().enqueue(new Callback<TourFragmentPlaceResponse>() {
+            @Override
+            public void onResponse(Call<TourFragmentPlaceResponse> call, Response<TourFragmentPlaceResponse> response) {
+                Log.d("udoLog", "추천 관광지 조회 body 내용 = " + response.body());
+                Log.d("udoLog", "추천 관광지 성공여부 = " + response.isSuccessful());
+                Log.d("udoLog", "추천 관광지 상태코드 = " + response.code());
+
+                //통신 성공
+                if (response.isSuccessful() && response.body() != null) {
+
+                    //response.body()를 result에 저장
+                    TourFragmentPlaceResponse result = response.body();
+
+                    //받은 코드 저장
+                    int resultCode = response.code();
+
+                    //맛집 조회 성공
+                    int success = 200;
+
+                    if (resultCode == success) {
+                        String id = result.getId();
+                        String dateTime = result.getDateTime();
+                        String message = result.getMessage();
+                        List<TourFragmentPlaceResponse.PlaceList> placeList = result.getPlaceList();
+
+                        //여행지 코스 전체 목록 조회 로그
+                        Log.d("udoLog", "추천 관광지 조회 = \n" +
+                                "Id: " + id + "\n" +
+                                "dateTime: " + dateTime + "\n" +
+                                "message: " + message + "\n" +
+                                "content" + placeList
+                        );
+
+                        if(placeList.toString() == "[]") {
+                            tourPlaceRecyclerView.setVisibility(View.INVISIBLE);
+                            noneTourPlaceText.setVisibility(View.VISIBLE);
+                        } else {
+                            tourPlaceRecyclerView.setVisibility(View.VISIBLE);
+                            noneTourPlaceText.setVisibility(View.INVISIBLE);
+
+                            //Recycler View 레이아웃 설정
+                            for (TourFragmentPlaceResponse.PlaceList place : placeList) {
+
+                                //맛집 리스트 상세 조회 로그
+                                Log.d("udoLog", "추천 관광지 리스트 = \n" +
+                                        "id: " + place.getId() + "\n" +
+                                        "placeName: " + place.getPlaceName() + "\n" +
+                                        "intro: " + place.getIntro() + "\n" +
+                                        "context: " + place.getContext() + "\n" +
+                                        "photo: " + place.getPhoto() + "\n"
+                                );
+                                tourFragmentPlaceAdapter.addItem(new TourFragmentPlaceListItem(place.getId(), place.getPlaceName(), place.getIntro(), place.getPhoto()));
+                            }
+                            tourPlaceRecyclerView.setAdapter(tourFragmentPlaceAdapter);
+                        }
+
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                        builder.setTitle("알림")
+                                .setMessage("추천 관광지를 조회할 수 없습니다.\n 다시 시도해주세요.")
+                                .setPositiveButton("확인", null)
+                                .create()
+                                .show();
+                    }
+                }
+            }
+
+            //통신 실패
+            @Override
+            public void onFailure(Call<TourFragmentPlaceResponse> call, Throwable t) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("알림")
+                        .setMessage("예기치 못한 오류가 발생하였습니다.\n 고객센터에 문의바랍니다.")
+                        .setPositiveButton("확인", null)
+                        .create()
+                        .show();
             }
         });
     }
